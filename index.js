a5d170368e1f1b6bb3fdaf6b142e695a0376e41b
0hyeon <djdjdjk2006@naver.com>
2021-06-07 23:23:34 +0900

    로그인&토큰생성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 
@@ -11,6 +12,7 @@ const { User } = require("./models/User")
 app.use(bodyParser.urlencoded({extended:true}));
 //application json 타입으로 된것을 분석해서 가져옴 
 app.use(bodyParser.json());
+app.use(cookieParser());//쿠키파서 사용 
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -32,6 +34,35 @@ app.post('/register',(req,res) => {
         })
     })//save는 mongodb에서 오는 메서드 
 })
+
+app.post('/login',(req,res) =>{
+    //요청한 이메일이 db에 있는지 찾는다. 
+    User.findOne({ email:req.body.email},(err,user)=>{//User db를가져옴 findOne은 몽고db에서 제공하는 메소드 이용
+        if(!user){
+            return res.json({
+                loginSuccess:false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            })
+        }
+        //요청된 이메일이 db에있다면 비밀번호가 맞는 비밀번호 인지 확인
+        user.comparePassword(req.body.password, (err,isMatch) => {
+            if(!isMatch)
+                return res.json({ loginSuccess:false, message:"비밀번호가 틀렸습니다."})
+
+            //비밀번호가 맞다면 그유저를위한 token생성, 토큰생성을 위해 npm install jsonwebtoken --save
+            user.generateToken((err,user) => {
+                if(err) return res.status(400).send(err);
+                
+                //토큰을 저장한다. 어디에? 쿠키,로컬스토리지
+                //npm install cookie-parser 필요 
+                res.cookie("x_auth",user.token)//쿠키가생성
+                .status(200)//성공
+                .json({ loginSuccess:true, userId: user._id})//json으로 데이터보내주면됨
+            })
+        })//comparePassword메소드를 만들어서 req.body,password랑 (err,Ismatch)를 넣어줌
+    })
+})
+
 app.listen(port,function(){
     console.log('express listening on port',port);
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');//암호화 시키는 패키지
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,52 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+userSchema.pre('save',function(next){//pre는 mongoose메서드 save 하기전 next로 보냄 
+    //비밀번호를 암호화 시킨다.
+    var user = this;
+    if(user.isModified('password')){//비밀번호 수정시에만 
+
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err)return next(err)//error가 나면 err로 보내주고요
+    
+            bcrypt.hash(user.password,salt,function(err,hash){
+                if(err) return next(err)//error가 나면 err로 보내주고요
+                
+                //암호화 성공했다면
+                user.password = hash//비밀번호를 hash로 바꿔줌
+                next()//돌아갸야죠 Userjs에user.save끝나는 지점으로
+    
+            })//제대로 생성했다면 (풀네임비밀번호,암호화패키지salt)  , hash는 암호화된내용 , this = 상수u serSchema를 가르킴 
+        })
+
+    }else{
+        next()//이게없으면 여기서 머무름
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    //plainPassword 1234567 , 암호화된 비밀번호 랑 비교를 해야
+    bcrypt.compare(plainPassword,this.password,function(err,isMatch){
+        if(err) return cb(err),//틀리면 err
+        cb(null,isMatch)//에러가없고(null), isMatch(true)이면  
+    })//index.js에서 comparePassword메서드를 사용가능할수있다.
+}
+
+//토큰생성
+userSchema.methods.generateToken = function(cb){//cb는 콜백
+    
+    var user = this;
+    //jsonwebtoken을 이용해서 tonken생성하기
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+    // user._id + 'secretToken'= token
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+
 const User = mongoose.model('User',userSchema)
 
 //다른데서도 쓸수있게 exports
