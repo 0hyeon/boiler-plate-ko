749944b89159c5f338799392cf6b201f65fbb339
0hyeon <djdjdjk2006@naver.com>
2021-06-16 00:38:10 +0900

    20210616

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ function LandingPage() {//LandingPage들어오자 마자 useEffect 실행
 
     useEffect(() => {
         axios.get('/api/hello')//get req를 서버에다 보내서 
-        .then(response => console.log(response.data))//서버에서 돌아오는 res를 콘솔창에 볼수있게 해주는것 
+        .then(response=> {console.log(response)})//서버에서 돌아오는 res를 콘솔창에 볼수있게 해주는것 
     }, [])
 
 
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,9 +1,10 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 module.exports = function(app){
     app.use(
-        '/api',
-        proxy({
-            target: 'http//localhost:5000',
+        '/api/hello',
+        createProxyMiddleware({
+            target: 'http://localhost:5000',
             changeOrigin:true,
         })
     );
